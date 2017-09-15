cfa9c544a89806379ef4b4d8f46d1552c610b905
Bill Wang <805801378@qq.com>
2017-09-15 09:38:15 +0800

    Deep_Inside_Convolutional_Networks

--- caffe_layer_visual/example.py
+++ caffe_layer_visual/example.py
@@ -9,7 +9,7 @@ import caffe
 
 # Load model
 net = caffe.Net('finetune_googlenet_newFood724_aug_test.prototxt',
-                '11_7_finetune_googlenet_newFood724_iter_100000.caffemodel',
+                '../11_7_finetune_googlenet_newFood724_iter_100000.caffemodel',
                 caffe.TEST)
 
 # visualize_weights(net, 'conv1/7x7_s2', filename='conv1.png')
