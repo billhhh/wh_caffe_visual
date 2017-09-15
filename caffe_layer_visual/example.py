78686a3ee44b1419151f74c1f92e264c35582c1b
Bill Wang <805801378@qq.com>
2017-09-15 08:07:04 +0800

    init

---
+++ caffe_layer_visual/example.py
@@ -0,0 +1,15 @@
+from visualize_caffe import *
+import sys
+
+# Make sure caffe can be found
+sys.path.append('D:/Projects/caffe-windows/caffe/python')
+
+import caffe
+
+
+# Load model
+net = caffe.Net('finetune_googlenet_newFood724_aug_test.prototxt',
+                '11_7_finetune_googlenet_newFood724_iter_100000.caffemodel',
+                caffe.TEST)
+
+visualize_weights(net, 'conv1/7x7_s2', filename='conv1.png')
