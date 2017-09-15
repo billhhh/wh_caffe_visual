6c8e495be328e7eb515e8bae232b6ad8772b1ba2
Bill Wang <805801378@qq.com>
2017-09-15 08:14:24 +0800

    1

--- caffe_layer_visual/example.py
+++ caffe_layer_visual/example.py
@@ -12,4 +12,5 @@ net = caffe.Net('finetune_googlenet_newFood724_aug_test.prototxt',
                 '11_7_finetune_googlenet_newFood724_iter_100000.caffemodel',
                 caffe.TEST)
 
-visualize_weights(net, 'conv1/7x7_s2', filename='conv1.png')
+# visualize_weights(net, 'conv1/7x7_s2', filename='conv1.png')
+visualize_weights(net, 'inception_5b/pool_proj', filename='')
